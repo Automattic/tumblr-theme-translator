d17f1d994ccd51d78caa3f29eb72fd4bc1c441c6
TommusRhodus <tom.rhodes@automattic.com>
2024-10-17 13:58:25 +0100

    Make Redux theme search work with WordPress

--- theme/tumblr3/functions.php
+++ theme/tumblr3/functions.php
@@ -12,12 +12,6 @@ defined( 'ABSPATH' ) || exit;
  * @return void
  */
 function tumblr3_theme_support(): void {
-	global $content_width;
-
-	if ( ! isset( $content_width ) ) {
-		$content_width = 600;
-	}
-
 	add_theme_support( 'post-formats', array( 'aside', 'image', 'gallery', 'link', 'audio', 'video', 'quote', 'chat' ) );
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'wp-block-styles' );
@@ -37,3 +31,35 @@ function tumblr3_enqueue_scripts(): void {
 	wp_enqueue_style( 'tumblr3-style', TUMBLR3_URL . 'assets/css/build/index.css', array(), TUMBLR3_METADATA['Version'] );
 }
 add_action( 'wp_enqueue_scripts', 'tumblr3_enqueue_scripts' );
+
+/**
+ * Add a new query variable for Tumblr search.
+ *
+ * @param array $vars Registered query variables.
+ *
+ * @return array
+ */
+function tumblr3_add_tumblr_search_var( $vars ): array {
+	$vars[] = 'q';
+	return $vars;
+}
+add_filter( 'query_vars', 'tumblr3_add_tumblr_search_var' );
+
+/**
+ * Redirect Tumblr search to core search.
+ *
+ * @param WP_Query $query The main query.
+ * @return void
+ */
+function tumblr3_redirect_tumblr_search( $query ): void {
+	// Check if it's the main query and not in the admin area
+	if ( $query->is_main_query() && ! is_admin() ) {
+
+		// If 'q' is set, redirect to the core search page.
+		if ( isset( $query->query_vars['q'] ) ) {
+			wp_safe_redirect( home_url( '/?s=' . $query->query_vars['q'] ) );
+			exit;
+		}
+	}
+}
+add_action( 'pre_get_posts', 'tumblr3_redirect_tumblr_search' );
